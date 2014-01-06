ae53e302aba0e140d4dc8c0b30d998cbc9bc1a9d
Ivan Trapeznikov <trapeznikoff@yandex.ru>
2014-01-06 23:42:27 +0400

    one template and some views added

---
+++ goods/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from goods import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<producer_id>\d+)/$', views.prod, name='prodname'),
+)
--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from goods.models import Producer,Product
+from django.template import RequestContext, loader
 
-# Create your views here.
+
+def index(request):
+    producer_list = Producer.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('goods/index.html')
+    output = ', '.join([p.name for p in producer_list])
+    context = RequestContext(request, {
+        'producers_list': producer_list,
+    })
+    return HttpResponse(template.render(context))
+
+def prod(request, producer_id):
+    return HttpResponse("You're looking goods of producer: %s." % producer_id)
+
+
+
+
