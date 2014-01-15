1eadd70319f4f23002cec4566b47646e7c2880cb
Ivan Trapeznikov <trapeznikoff@yandex.ru>
2014-01-15 20:36:09 +0400

    continue trying with m10g with forms

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from users.models import User
+from users.models import User,MyUserManager
 from users.forms import UserForm
 
 # Create your views here.
@@ -9,11 +9,11 @@ from users.forms import UserForm
 def userform(request):
     if request.method == 'POST': # If the form has been submitted...
         form = UserForm(request.POST) # A form bound to the POST data
-        user_name = form.cleaned_data['user_name']
-        password = form.cleaned_data['password']
-        User.create(user_name, password)
-        User.save()
-        return HttpResponseRedirect('/users_list/') # Redirect after POST
+        if form.is_valid():
+            user_name = form.cleaned_data['user_name']
+            password = form.cleaned_data['password']
+            User.objects.create_user(user_name,password)
+            return HttpResponseRedirect('/users_list/') # Redirect after POST
     else:
         form = UserForm()  # An unbound form
     return render(request, 'users/login.html', {'form': form,})
