c017aaff3524e3f78615ad575c4c1da91d9d9dd9
Maxim Schepelin <m.schepelin@gmail.com>
2014-01-18 11:34:34 +0300

    templates moves

--- goods/views.py
+++ goods/views.py
@@ -1,21 +1,21 @@
 from django.http import HttpResponse
-from goods.models import Producer,Product
+from goods.models import Producer, Product
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     producer_list = Producer.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('goods/index.html')
+    template = loader.get_template('catalog/index.html')
     context = RequestContext(request, {
         'producers_list': producer_list,
     })
     return HttpResponse(template.render(context))
 
+
 def producer_list(request, producer_id):
     producer = get_object_or_404(Producer, pk=producer_id)
     return render(request, 'goods/producer_list.html', {'producer': producer})
-
 
 
 def description(request, product_id):
@@ -25,12 +25,4 @@ def description(request, product_id):
 
 def all_goods(request):
     product_list = Product.objects.all()
-    return render(request, 'goods/all_goods_list.html', {'product_list' : product_list,})
-
-
-
-
-
-
-
-
+    return render(request, 'goods/all_goods_list.html', {'product_list': product_list})
