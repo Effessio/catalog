f8b0746b67e550325b3ea29cf3dbe632d47c96b0
Maxim Schepelin <m.schepelin@gmail.com>
2014-01-19 13:01:39 +0300

    login and logout in base template

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from users.models import User
 from users.forms import UserForm
 from django.contrib.auth import authenticate, login, logout
@@ -8,16 +8,17 @@ from django.contrib.auth import authenticate, login, logout
 
 
 def userform(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = UserForm(request.POST) # A form bound to the POST data
+    if request.method == 'POST':
+        form = UserForm(request.POST)
         if form.is_valid():
             user_name = form.cleaned_data['user_name']
             password = form.cleaned_data['password']
-            User.objects.create_user(user_name,password)
-            return HttpResponseRedirect('users_list') # Redirect after POST
+            User.objects.create_user(user_name, password)
+            return HttpResponseRedirect('users_list')
     else:
         form = UserForm()  # An unbound form
-    return render(request, 'users/register.html', {'form': form,})
+    return render(request, 'users/register.html', {'form': form})
+
 
 def users_list(request):
     users_list = User.objects.all()
@@ -25,8 +26,8 @@ def users_list(request):
 
 
 def userlogin(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = UserForm(request.POST) # A form bound to the POST data
+    if request.method == 'POST':
+        form = UserForm(request.POST)
         if form.is_valid():
             user_name = form.cleaned_data['user_name']
             password = form.cleaned_data['password']
@@ -35,10 +36,11 @@ def userlogin(request):
                 return HttpResponseRedirect('bad')
             else:
                 login(request, user)
-                return HttpResponseRedirect('/goods/all')
+                return HttpResponseRedirect('/goods')
     else:
         form = UserForm()
-    return render(request, 'users/register.html', {'form': form,})
+    return render(request, 'users/register.html', {'form': form})
+
 
 def bad(request):
     return render(request, 'users/bad.html')
@@ -46,5 +48,4 @@ def bad(request):
 
 def userlogout(request):
     logout(request)
-    HttpResponseRedirect('goods/all')
-
+    HttpResponseRedirect('/goods')
