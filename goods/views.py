f87309f1f69eef8aad4734fed6665e576ab93659
Ivan Trapeznikov <trapeznikoff@yandex.ru>
2014-01-20 20:08:44 +0400

    addproduct finished with negative cases

--- goods/views.py
+++ goods/views.py
@@ -31,37 +31,21 @@ def all_goods(request):
 
 def add_product(request, producer_id):
     producer = get_object_or_404(Producer, pk=producer_id)
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.producer = producer
-            product.save()
-            return HttpResponseRedirect(product.get_absolute_url())
-    else:
-        form = ProductForm()
-        return render(request, 'goods/add_product.html', {'form': form})
-
-
-
-
-
-
-
-"""if request.user.is_authenticated:
-            try:
-                Producer.objects.get(id = producer_id)
-
-            except Producer.DoesNotExist:
-                errormessage='producer does not exist'
+    if request.user.is_authenticated():
+        if producer.moderator == request.user:
+            if request.method == 'POST':
+                form = ProductForm(request.POST)
+                if form.is_valid():
+                    product = form.save(commit=False)
+                    product.producer = producer
+                    product.save()
+                    return HttpResponseRedirect(product.get_absolute_url())
             else:
-                if request.user.producer_id == producer_id:
-                    form = ProductForm(request.POST)
-                    product = form.save()
-                    return HttpResponseRedirect(product.get_absolute_url)
-                else:
-                    errormessage = 'you do not have a permission to add goods to this producer'
+                form = ProductForm()
+                return render(request, 'goods/add_product.html', {'form': form})
         else:
-            errormessage='user is not authentificated'
-    return render(request, 'goods/add_product.html', {'product_id':product_id,
-                                                     "error")"""
+            error_message = "you don't have permission to access this page"
+            return render(request, 'goods/error.html', {'error_message': error_message})
+    else:
+        error_message = 'you need to login to access this page'
+        return render(request, 'goods/error.html', {'error_message': error_message})
